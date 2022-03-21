7d31c725aaebb84e3282c7cd794af942b42d733f
ebruKosker <ebrukosker123@gmail.com>
2022-03-21 21:39:00 +0300

    odev1 eklendi

---
+++ Ders1/Odev1/intro/src/intro/main.java
@@ -0,0 +1,21 @@
+package intro;
+
+public class main {
+
+	public static void main(String[] args) {
+		String ilkelVeriTipi[]= {"byte","short","int","long","char","float","double"};
+		String referansVeriTipi[]= {"class","interface","array"};
+		String nullVeriTipi[]= {"null"};
+		for(String veri:ilkelVeriTipi) {
+			System.out.println(veri);
+		}
+		for(String veri:referansVeriTipi) {
+			System.out.println(veri);
+		}
+		for(String veri:nullVeriTipi) {
+			System.out.println(veri);
+		}
+		
+	}
+
+}
