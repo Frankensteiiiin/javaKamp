4c086b64922a5e3ccbea5474e4a46aa2f3002be6
ebruKosker <ebrukosker123@gmail.com>
2022-03-21 22:13:45 +0300

    5. ders uygulandı

---
+++ Ders1/Odev3/helloJava/src/helloJava/javaHelloWorld.java
@@ -0,0 +1,10 @@
+package helloJava;
+
+public class javaHelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java2");
+	}
+
+}
