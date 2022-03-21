d3716eca3cb37832b99a122b323d56855fff8df6
ebruKosker <ebrukosker123@gmail.com>
2022-03-21 23:58:51 +0300

    19. ders uygulandı

---
+++ Ders1/Odev3/asalSayi/src/asalSayi/asalSayi.java
@@ -0,0 +1,22 @@
+package asalSayi;
+
+public class asalSayi {
+
+	public static void main(String[] args) {
+		int number=24;
+		int number1=number/2;
+		int a=0;
+		for(int i=2;i<=number1;i++) {
+			if(number%i==0) {
+				System.out.println("sayı asal değil");
+				a=1;
+				break;
+			}
+				
+		}
+		if(a==0) {
+			System.out.println("sayı asal sayıdır");
+		}
+	}
+
+}
