7087345d095a1b978e55e4fc70e088936b969fc6
ebruKosker <ebrukosker123@gmail.com>
2022-03-21 23:36:15 +0300

    ders 14 uygulandı

---
+++ Ders1/Odev3/arraysDemo/src/arraysDemo/arraysDemo.java
@@ -0,0 +1,12 @@
+package arraysDemo;
+
+public class arraysDemo {
+
+	public static void main(String[] args) {
+		String[] ogrenciler= {"engin","derin","salih","ekin"};
+		for(String ogrenci:ogrenciler) {
+			System.out.println(ogrenci);
+		}
+	}
+
+}
