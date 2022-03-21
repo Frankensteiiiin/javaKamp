97717c5aa1801f6f13f9a39e20dbe9701f51758c
ebruKosker <ebrukosker123@gmail.com>
2022-03-21 22:24:33 +0300

    6. ders uygulandı

---
+++ Ders1/Odev3/variables/src/variables/variables.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int ogrenciSayi=12;
+		String mesaj="Öðrenci Sayýsý = ";
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+
+	}
+
+}
